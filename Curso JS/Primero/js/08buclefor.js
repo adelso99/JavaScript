19acc332b6099a59bd49be1e604f97a673c2093a
unknown <ahernandezg32@miumg.edu.gt>
2024-02-02 22:13:56 -0600

    bucle for

---
+++ Curso JS/Primero/js/08buclefor.js
@@ -0,0 +1,12 @@
+'use trict'
+// BUCLE FOR
+// BUCLE ES UNA STRUCTURA DE CONTROL QUE SE REPITE VARIAS VECES
+
+var numero = 100;
+
+for(var i = 0; i <= numero; i++){
+    console.log("Vamos por el Numero: "+i);
+
+    debugger;
+}
+
