5aa43fb6c5a35ee15266a064604e1dc903fcde72
unknown <ahernandezg32@miumg.edu.gt>
2024-02-04 23:51:56 -0600

    Paso 2

---
+++ Curso JS/Primero/js/09while.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// BUCLE WHILE significa mientras
+
+var year = 2018;
+while(year != 1991){
+        // EJECUTA ESTO
+        console.log("Estamos en el año: "+year);
+
+        if(year == 2000){
+            break;
+        }
+
+        year--;
+
+}
+
+// DO WHILE
+var years = 30;
+
+do{
+alert("Solo cuando sea diferente a 20");
+years--;
+
+}while(years > 25)
---
+++ Curso JS/Primero/js/10ventanas.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// ALERTA
+
+   // alert("esta es mi alerta!!");
+    // alert("Este es mi texto");
+
+// CONFIRMACION
+// var mi_resultado = confirm("Estas seguro de querer continuar?");
+// console.log(mi_resultado);
+
+// INGRESO DATOS
+var mi_resultado = prompt("Que edad tienes?", 18);
+console.log(typeof mi_resultado);
