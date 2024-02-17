b4f95df570682d1f9b42e038bc00e4ceb897f5c9
adelso <adelso@gmail.com>
2024-02-17 00:29:21 -0600

    function

---
+++ Curso JS/Primero/js/19funciones.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// FUNCIONES
+// una funcion es una agrupacion reutilizable de un conjunto de instrucciones 
+
+
+//defino la funcion
+function calculadora(){
+    // conjunto de instrucciones a ejecutar 
+    console.log("hola soy la calculadora");
+    console.log("si soy yo");
+
+    return "hola soy la calculadora!!";
+
+}
+//console.log(calculadora(), calculadora(), calculadora());
+
+//invocar o llamar la funcion
+var resultado = calculadora();
+
+console.log(resultado);
