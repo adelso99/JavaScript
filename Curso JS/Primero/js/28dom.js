9ecbb76a4d97625f0e12ebe32bd33ee3fa83432a
adelso99 <adelsohernandez99@gmail.com>
2024-04-05 00:20:45 -0600

    ar

--- Curso JS/Primero/js/28dom.js
+++ Curso JS/Primero/js/28dom.js
@@ -15,4 +15,31 @@ caja.style.padding = "20px";
 caja.style.color = "red"; 
 caja.className = "Hola 2";
 
+
+// Conseguir elementos por su etiqueta
+var todoslosdivs= document.getElementsByTagName('div');
+
+//todoslosdivs.forEach((valor, indice) => {
+var seccion = document.querySelector("#miseccion");
+var hr = document.createElement("hr");
+
+var valor;
+for(valor in todoslosdivs){
+ if(typeof todoslosdivs[valor].textContent == 'string'){
+    var parrafo = document.createElement("p");
+    var texto = document.createTextNode(todoslosdivs[valor].textContent);
+    parrafo.append(texto);
+    seccion.append(parrafo);
+    }
+}
+seccion.append(hr);
+
+//var contenidoentexto = todoslosdivs[2].textContent;
+//contenidoentexto.innerHTML = "Nuevo texto";
+
+//console.log(contenidoentexto);
+
+
+
+// Conseguir elementos por su clase css
 console.log(caja);
