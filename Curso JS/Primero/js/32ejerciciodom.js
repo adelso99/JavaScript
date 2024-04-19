4755581e24b1b84640c2f4075900cb15e2cadea7
adelso99 <adelsohernandez99@gmail.com>
2024-04-19 00:17:23 -0600

    ejec

---
+++ Curso JS/Primero/js/32ejerciciodom.js
@@ -0,0 +1,47 @@
+'use strict'
+
+window.addEventListener('load', function(){
+console.log("DOM cargado");
+
+ var Formulario = document.querySelector("#Formulario");
+ var box_dashed = document.querySelector(".dashed");
+ box_dashed.style.display = "none";
+
+ Formulario.addEventListener('submit', function(){
+    console.log("Evento Submit capturado");
+
+    var nombre = document.querySelector('#nombre').value;
+    var apellidos = document.querySelector('#apellidos').value;
+    var edad = document.querySelector('#edad').value;
+
+    box_dashed.style.display = "block";
+    var p_nombre = document.querySelector("#p_nombre span");
+    var p_apellidos = document.querySelector("#p_apellidos span");
+    var p_edad = document.querySelector("#p_edad span");
+
+    p_nombre.innerHTML = nombre;
+    p_apellidos.innerHTML = apellidos;
+    p_edad.innerHTML = edad;
+
+    /*
+    var datos_usuario = [nombre, apellidos, edad];
+    var indice;
+    for(indice in datos_usuario){
+        var parrafo = document.createElement("p");
+        parrafo.append(datos_usuario[indice]);
+         box_dashed.append(parrafo);
+    }
+       */
+    });
+});
+// 1. Formulario con los Campos Nombre, Apellidos y Edad 
+
+
+
+
+//2. Boton de enviar el formulario: Eventos Submit
+
+//3. Mostrar datos por pantalla
+
+//4. Tener un Boton que al darle click nos muestra los
+//datos actualces del formulario
