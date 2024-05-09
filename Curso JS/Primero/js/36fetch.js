c22059f38223cecba53c62c8c0defce8b9a61578
adelso99 <adelsohernandez99@gmail.com>
2024-05-09 00:18:58 -0600

    sub

--- Curso JS/Primero/js/36fetch.js
+++ Curso JS/Primero/js/36fetch.js
@@ -3,21 +3,50 @@
 // Accede a un servicio remoto para los dato
 
 var div_usuarios = document.querySelector("#usuarios");
+var div_janet = document.querySelector("#janet");
 
-var usuarios =  [];
-
-fetch('https://reqres.in/api/users')
+getUsuarios()
     .then(data => data.json())
     .then(users => {
-        usuarios = users.data;
-        console.log(usuarios);
+       listadoUsuarios(users.data);
+
+       return getJanet();
+    })
+    .then(data => data.json())   
+    .then(user => {   
+        mostrarJanet(user.data);
+    }); 
+
+
+    function getUsuarios(){
+        return fetch('https://reqres.in/api/users');
+    }
         
-        usuarios.map((user, i) => {
-                let nombre = document.createElement('h3');
-                nombre.innerHTML = i + '.'+ user.first_name + " " + user.last_name;
-                div_usuarios.appendChild(nombre);
-                document.querySelector(".loading").style.display = 'none';
+    function getJanet(){
+        return fetch('https://reqres.in/api//api/users/2');
+    }
+    
+  function listadoUsuarios(usuarios){
+     usuarios.map((user, i) => {
+      let nombre = document.createElement('h3');
+      nombre.innerHTML = i + '.'+ user.first_name + " " + user.last_name;
+      div_usuarios.appendChild(nombre);
+      document.querySelector(".loading").style.display = 'none';
         });
 
-    });
-    
+    }     
+  
+    function mostrarJanet(user){
+        console.log(user);
+        let nombre = document.createElement('h4');
+        let avatar = document.createElement('img');
+        nombre.innerHTML = user.first_name + " " + user.last_name;
+        avatar.src = user.avatar;
+        avatar.width = '100';
+         div_janet.appendChild(nombre);
+         div_janet.appendChild(avatar);
+         document.querySelector("#janet .loading").style.display = 'none';
+        
+       }     
+     
+       
