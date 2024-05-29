a537d5fe706d1568e5c2826b2ea6c08053254a6b
adelso99 <adelsohernandez99@gmail.com>
2024-05-29 00:01:49 -0600

    jquery

---
+++ Curso JS/Primero/js/38fechas.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var fecha = new Date();
+
+var year = fecha.getFullYear();
+var mes = fecha.getMonth();
+var dia = fecha.getDate();
+var hora = fecha.getHours();
+var minutos = fecha.getMinutes();
+var segundos = fecha.getSeconds();
+
+var textoHora = `
+    El año es: ${year}
+    El mes es: ${mes}
+    El dia es: ${dia}
+    El hora es: ${hora}
+    Los minutos son: ${minutos}
+    Los segundo son: ${segundos}
+`;
+
+console.log(textoHora);
+
+//Funciones Matematicas de JS
+// Google: Javascript Math
+// Math es numero aliatorio
+// Math.celi es para numeros enteros sin decimales
+console.log(Math.ceil(Math.random()*10000));
